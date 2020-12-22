6610bfc0388e21828d5045d1c8cdb3d567ec44c5
4piu <16668056@users.noreply.github.com>
2020-12-22 18:33:25 +0800

    change port

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 pre_shared_key: bytes = b"ARRRH YOU READY KIDS???"
 enable_gzip: bool = True
 file_block_size: int = 8000000  # Bytes
-listen_port: int = 52201
+listen_port: int = 25252
 concurrent_downloading: int = 8
